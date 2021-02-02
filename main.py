8b7fd05b70ba6a58bd9095abb8a38fb9085436a0
otwald <hinterlanddev@googlemail.com>
2021-02-02 13:14:36 +0100

    notfdall patch weil muss

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from person import Person
 from db_handler import DataBase
 
+
 def main():
     p = Person()
     p.generatePerson()
     print(p.body)
+# qwedqwdwd
 
 
 if __name__ == "__main__":
